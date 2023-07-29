53363bc944b7713ad1301ac5afa00a2938c82ef3
Gian Paucar Cortez <77996469+gian-pc@users.noreply.github.com>
2023-07-29 00:15:24 -0500

    IoC actualizado

--- SpringUno/src/main/java/es/pildoras/IoC/UsoEmpleados.java
+++ SpringUno/src/main/java/es/pildoras/IoC/UsoEmpleados.java
@@ -1,22 +1,43 @@
 package es.pildoras.IoC;
 
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class UsoEmpleados {
     public static void main(String[] args) {
 
-        // Creación de objetos de tipo empleado
+// --------------------------  Sin IoC  ---------------------------------
 
-        JefeEmpleado Empleado1 = new JefeEmpleado(); // sin interfaces
-        Empleados Empleado2 = new JefeEmpleado(); // con interfaces
-        Empleados Empleado3 = new SecretarioEmpleado(); // con interfaces
-        Empleados Empleado4 = new DirectorEmpleado(); // con interfaces
+        // Creación de objetos de tipo empleado
+        JefeEmpleado jefeEmpleado = new JefeEmpleado(); // sin interfaces
+
+        Empleados jefeEmpleado2 = new JefeEmpleado(); // con interfaces
+        Empleados secretarioEmpleado = new SecretarioEmpleado(); // con interfaces
+        Empleados directorEmpleado = new DirectorEmpleado(); // con interfaces
 
         // Uso de los objetos creados
+        System.out.println(jefeEmpleado.getTareas());
+        System.out.println(jefeEmpleado2.getTareas());
+        System.out.println(secretarioEmpleado.getTareas());
+        System.out.println(directorEmpleado.getTareas());
 
-        System.out.println(Empleado1.getTareas());
-        System.out.println(Empleado2.getTareas());
-        System.out.println(Empleado3.getTareas());
-        System.out.println(Empleado4.getTareas());
+
+
+// --------------------------  Con IoC  ---------------------------------
+
+        // 1 Paso: Crear el contexto; nos permite cargar el archivo xml
+        ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // 2 Paso: Pedir el bean
+        Empleados Gian = contexto.getBean("miEmpleado",Empleados.class);
+
+        // 3 Paso: Utilizar este objeto(Bean)
+        System.out.println();
+        System.out.println(Gian.getTareas());
+
+        // 4 Paso: Cerrar el XML
+        contexto.close();
+
 
 
 
